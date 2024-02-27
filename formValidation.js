00515c5a82cfbb61b89b970352b0e01ccfb3c5d7
merttru <mariatrufal@gmail.com>
2024-02-27 21:45:20 +0000

    hola commit

--- formValidation.js
+++ formValidation.js
@@ -1,35 +1,38 @@
 const formulario = document.getElementById("formulario");
 
 const lettersRegex = /^[a-zA-Z\s]+$/;
+const emailRegex = /^.+@.+..+$/;
 formulario.addEventListener("submit", (e) => {
-  e.preventDefault();
   const [nameElement, lastNameElement, ageElement, emailElement, subjectElement, textElement] = e.target;
 
   const name = nameElement.value;
   if ((name.length < 3 || name.length > 14) && !lettersRegex.test(name)) {
-    console.log("Error");
+    alert("Debe ser extensión de 3 letras y menor de 14 y solo se permitirán letras.")
   }
 
   const lastName = lastNameElement.value;
   if((lastName.length <=3 || lastName.length >=25) && !lettersRegex.test(lastName)){
-    console.log("Error ")
+    alert("Debe ser mayor de 4 letras y menor de 30 y solo se permitirán letras. ")
   }
   const age = ageElement.value;
   if(isNaN(age) || age < 10  || age > 99){
     alert("La edad debe ser un número y estar entre 10 y 99 años.");
-    return;
   }
 
   const email = emailElement.value;
-  if((emailElement)){
-
+  if((!emailElement.test(email))){
+    alert("Ingrese una dirección de correo electrónico válida.")
   }
   const subject = subjectElement.value;
   if((subject.length >40)){
-    console.log("Error")
+    alert("Hasta 40 caracteres")
   }
-  if((textElement.length >400)){
-    console.log("Error")
+  const text =  textElement.value;
+  const cleanedText = espacioblanco(text)
+  if(cleanedText.split(" ").length > 400){
+    alert("El mensaje no puede superar los 400 palabras.")
   }
-
 });
+function espacioblanco(text){
+  return text.replace(/\s+/g," "); /*Sutituira todas las coincidencias del regex por espacio, podemos poner replaceAll cuando queremos buscar todas las palabras*/
+}
